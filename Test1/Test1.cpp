cc725fcd66d5e7332ad66cc5ab31e50808bc6601
Mohammed <m.saleh.tum@gmail.com>
2024-03-11 23:42:42 +0100

    Create Repo

---
+++ Test1/Test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "class1.h"
+#include "class2.h"
+using namespace std;
+
+int main()
+{
+    class1::print();
+    class2::print();
+}
---
+++ Test1/class1.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class class1
+{
+public:
+	static void print() {
+		cout << "Hallo" <<endl;
+	}
+};
+
